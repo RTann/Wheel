fb3d8b240cb09cb2481f03fb21cb15fc51c6b435
RTann <rosstannenbaum@rosstannenbaum.local>
2014-05-26 14:21:28 -0400

    2nd Commit
    Started Mechanics of Simulation

--- src/physics/Mechanics.java
+++ src/physics/Mechanics.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package physics;
 
 /**
@@ -11,5 +5,30 @@ package physics;
  * @author rosstannenbaum
  */
 public class Mechanics {
+    
+    /* Acceleration due to gravity on Earth */
+    private static final double G = 9.81;
+    /* Coefficient of static friction */
+    private static final double MU = .5;
+    
+    /**
+     * Computes force exerted onto Wheel
+     * 
+     * @param start Start time
+     * @param end End time
+     * @return Force in Newtons
+     */
+    public int getForce(long start, long end) {
+        return (int) (end - start) / 100;
+    }
+    
+    /**
+     * 
+     * @param normal Normal force of Wheel on ground
+     * @return Force of Static Friction
+     */
+    public int getFriction(long normal) {
+        return (int) (normal * MU);
+    }
     
 }
