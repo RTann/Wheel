7a51263514515a927b6309a7d37c945f2fbebfa8
RTann <rosstannenbaum@rosstannenbaum.local>
2014-05-28 14:22:57 -0400

    Comments and Friction Attempt

--- src/graphics/Panel.java
+++ src/graphics/Panel.java
@@ -14,23 +14,40 @@ import javax.swing.Timer;
 import physics.Mechanics;
 
 /**
+ * Simulates motion of a wheel pushed by a force
  *
- * @author rosstannenbaum
+ * @author Ross Tannenbaum and Michael Francis
  */
 public class Panel extends JPanel implements ActionListener, MouseListener {
 
+    /* Dimension on screen in pixels */
     private static Dimension screensize;
+    /* height and width of screen in pixels */
     private static int height, width;
+    /* Timer to trigger actionPerformed() */
     private static Timer delay;
+    /* Indiciates if ball is moving */
     private static boolean ballIsMoving;
+    /* Coordinates of mouse press */
     private static int mouseX, mouseY;
+    /* Real time when mouse is pressed and held */
     private static long pressedTime, releasedTime;
-    private static int mass = 2;
-    private static int force;
+    /* Mass of Wheel. 1kg minimum */
+    private static final int mass = 2;
+    /* Forces exerted on Wheel */
+    private static int force, friction;
+    /* Acceleration of Wheel */
+    private static double acceleration;
+    /* Velocities of Wheel */
     private static double prevVelocity, currentVelocity;
+    /* Positions of Wheel */
     private static int xPrevPosition, xPosition;
-    private static int deltaT = 1;
+    /* Change in time for each call to actionPerformed() */
+    private static final int deltaT = 1;
 
+    /**
+     * Constructs a Panel Object
+     */
     public Panel() {
         delay = new Timer(0, this);
         screensize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -43,6 +60,11 @@ public class Panel extends JPanel implements ActionListener, MouseListener {
         setFocusable(true);
     }
 
+    /**
+     * Paint current image of Simulation
+     *
+     * @param g Graphics
+     */
     @Override
     public void paint(Graphics g) {
         super.paint(g);
@@ -52,24 +74,44 @@ public class Panel extends JPanel implements ActionListener, MouseListener {
         g.fillArc(xPosition, height - 600, 400, 400, 0, 360);
     }
 
+    /**
+     * Update simulation based on ActionEvent e
+     *
+     * @param e Event to indicate change in Simulation
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         prevVelocity = currentVelocity;
-        currentVelocity = Mechanics.getVelocity(prevVelocity, 0, deltaT);
-        xPrevPosition = xPosition;
-        xPosition = Mechanics.getPosition(xPrevPosition, currentVelocity, 0,
+        currentVelocity = Mechanics.getVelocity(prevVelocity, acceleration,
                 deltaT);
+        xPrevPosition = xPosition;
+        if (currentVelocity <= 0) {
+            ballIsMoving = false;
+            delay.stop();
+            return;
+        }
+        xPosition = Mechanics.getPosition(xPrevPosition, currentVelocity,
+                acceleration, deltaT);
         if (xPosition >= width) {
             xPosition = -400;
         }
         repaint();
     }
 
+    /**
+     * Not implemented
+     *
+     * @param e
+     */
     @Override
     public void mouseClicked(MouseEvent e) {
-        //Not implemented
     }
 
+    /**
+     * Utilizes information based on location and time of mouse press
+     *
+     * @param e Mouse is pressed
+     */
     @Override
     public void mousePressed(MouseEvent e) {
         if (!ballIsMoving) {
@@ -79,6 +121,11 @@ public class Panel extends JPanel implements ActionListener, MouseListener {
         }
     }
 
+    /**
+     * Utilizes information based on time of release and time relative to press
+     *
+     * @param e Mouse is released
+     */
     @Override
     public void mouseReleased(MouseEvent e) {
         if (!ballIsMoving) {
@@ -87,20 +134,36 @@ public class Panel extends JPanel implements ActionListener, MouseListener {
             force = Mechanics.getForce(pressedTime, releasedTime);
             prevVelocity = Mechanics.getVelocity(force, mass);
             currentVelocity = prevVelocity;
+            friction = Mechanics.getFriction(mass);
+            acceleration = 0/*-Mechanics.getAcceleration(friction, mass)*/;
             delay.start();
         }
     }
 
+    /**
+     * Not implemented
+     *
+     * @param e
+     */
     @Override
     public void mouseEntered(MouseEvent e) {
-        //Not implemented
     }
 
+    /**
+     * Not implemented
+     *
+     * @param e
+     */
     @Override
     public void mouseExited(MouseEvent e) {
-        //Not impletemented
     }
 
+    /**
+     * Runs Simulation
+     *
+     * @param args Most likely will be implemented to indicate how program will
+     * run (ex. with or without friction)
+     */
     public static void main(String[] args) {
         Panel p = new Panel();
         JFrame jf = new JFrame();
--- src/physics/Mechanics.java
+++ src/physics/Mechanics.java
@@ -1,55 +1,93 @@
 package physics;
 
 /**
+ * Basic Newtonian Mechanics functions to be used for Wheel simulation
  *
- * @author rosstannenbaum
+ * @author Ross Tannenbaum and Michael Francis
  */
-public class Mechanics {
+public final class Mechanics {
 
     /* Acceleration due to gravity on Earth */
     private static final double G = 9.81;
     /* Impulse time */
-    private static final double T = 1;
+    private static final double T = .1;
+    /* Coefficient of Static Friction */
+    private static final double MU = .004;
+
+    private Mechanics() {
+    }
 
     /**
-     * Computes force exerted onto Wheel (minimum force of 5 Newtons).
+     * Computes force exerted on Wheel (minimum force of 10 Newtons).
      *
      * @param start Start time
      * @param end End time
-     * @return Force in Newtons
+     * @return Force exerted on Wheel in Newtons
      */
     public static int getForce(long start, long end) {
-        int force = (int) Math.ceil((end - start) / 100);
-        return force <= 1 ? 1 : force;
+        int force = (int) Math.ceil((end - start) / 10);
+        return force <= 10 ? 10 : force;
     }
 
     /**
-     * Compute Force of Static Friction of Ground on Wheel
+     * Computes Force of Static Friction exerted on Wheel by Ground
      *
-     * @param normal Normal force of Wheel on ground
-     * @return Force of Static Friction
+     * @param mass mass of Wheel
+     * @return Force of Static Friction on Wheel in Newtons
      */
-    public static int getFriction(long normal) {
-        //TODO
-        return 0;
+    public static int getFriction(double mass) {
+        return (int) Math.ceil(mass * G * MU);
     }
 
+    /**
+     * Computes Acceleration of Wheel
+     *
+     * @param force Force exerted on Wheel in Newtons
+     * @param mass Mass of Wheel in kilograms
+     * @return Acceleration in meters per squared second
+     */
     public static double getAcceleration(int force, double mass) {
         return force / mass;
     }
 
+    /**
+     * Computes Velocity of Wheel
+     *
+     * @param force Force exerted on Wheel in Newtons
+     * @param mass Mass of Wheel in kilograms
+     * @return Velocity of Wheel in meters per second
+     */
     public static double getVelocity(int force, double mass) {
+        //Impulse = change in momentum
         return force * T / mass;
     }
 
+    /**
+     * Computes Velocity of Wheel
+     *
+     * @param initialVelocity Initial velocity of Wheel in meters per second
+     * @param acceleration Acceleration of Wheel in meters per squared second
+     * @param time Change in time in seconds
+     * @return Velocity of Wheel in meters per second
+     */
     public static double getVelocity(double initialVelocity,
-            double acceleration, int time) {
+            double acceleration, double time) {
         return initialVelocity + acceleration * time;
     }
 
+    /**
+     * Computes Position of Wheel
+     *
+     * @param initialPosition Initial position of Wheel in meters
+     * @param currentVelocity Initial velocity of Wheel in meters per second
+     * @param acceleration Acceleration of Wheel in meters per squared second
+     * @param time Change in time in seconds
+     * @return Position of Wheel in meters
+     */
     public static int getPosition(int initialPosition, double currentVelocity,
             double acceleration, int time) {
         return (int) Math.ceil(initialPosition + currentVelocity * time
                 + .5 * acceleration * Math.pow(time, 2));
     }
+
 }
